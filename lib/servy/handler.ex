c7c32eea34918d071549e9cef8346a4cc3cdea06
MeLeRyZ <meleryz@gmail.com>
2018-05-01 16:09:24 +0300

    rewrite routes functions add checkers to main pipeline

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,10 +1,29 @@
 defmodule Servy.Handler do
+
     def handle(request) do
         request
         |> parse
+        |> rewrite_path
+        |> log
         |> route
+        |> track
         |> format_response
     end
+
+    def track(%{ status: 404, path: path } = conv) do
+        IO.puts "Warning: #{path} is in /dev/null"
+        conv
+    end
+
+    def track(conv), do: conv
+
+    def rewrite_path(%{path: "/wildlife" } = conv) do
+        %{ conv | path: "/wildthings"}
+    end
+
+    def rewrite_path(conv), do: conv
+
+    def log(conv),  do:    IO.inspect conv
 
     def parse(request) do
         [method, path, _] =
@@ -12,23 +31,53 @@ defmodule Servy.Handler do
                     |> String.split("\n")
                     |> List.first
                     |> String.split(" ")
-        %{ method: method, path: path, resp_body: ""}
+        %{ method: method,
+              path: path,
+              resp_body: "",
+              status: nil}
     end
 
-    def route(conv) do
-        %{ conv | resp_body: "Bears, Lions, Pigeons"}
-        #conv = %{ method: "GET", path: "/wildthings", resp_body: }
-        # > conv.path - only works if the key is atom
+    # def route(conv) do
+    #     route(conv, conv.method,  conv.path)
+    #     #conv = %{ method: "GET", path: "/wildthings", resp_body: }
+    #     # > conv.path - only works if the key is atom
+    # end
+
+    def route(%{method: "GET", path: "/wildthings"} = conv) do
+        %{ conv | status: 200, resp_body: "Bears, Lions, Pigeons"}
+    end
+
+    def route(%{method: "GET", path: "/bears"} = conv) do
+        %{ conv | status: 200, resp_body: "Teddy, Slooney, Redditon"}
+    end
+
+    def route(%{method: "GET", path: "/bears/" <> id} = conv) do
+        %{ conv | status: 200, resp_body: "Bear #{id}"}
+    end
+
+    def route(%{ path: path} = conv) do
+        %{ conv | status: 404, resp_body: "No #{path} here!"}
     end
 
     def format_response(conv) do
         """
-        HTTP/1.1 200 OK
+        HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
         Content-Type: text/html
         Content-Length: #{String.length(conv.resp_body) }
 
         #{conv.resp_body}
         """
+    end
+
+    defp status_reason(code) do
+        %{
+            200 => "OK",
+            201 => "Created",
+            401 => "Unauthorized",
+            403 => "Forbidden",
+            404 => "Not Found",
+            500 => "Internal Server Error"
+        }[code]
     end
 
 end
@@ -39,6 +88,32 @@ Host: example.com
 User-agent: ExampleBrowser/1.0
 Accept: */*
 """
+response = Servy.Handler.handle(request)
+IO.puts response
 
+request = """
+GET /bears HTTP/1.1
+Host: example.com
+User-agent: ExampleBrowser/1.0
+Accept: */*
+"""
+response = Servy.Handler.handle(request)
+IO.puts response
+
+request = """
+GET /bears/1 HTTP/1.1
+Host: example.com
+User-agent: ExampleBrowser/1.0
+Accept: */*
+"""
+response = Servy.Handler.handle(request)
+IO.puts response
+
+request = """
+GET /wildlife HTTP/1.1
+Host: example.com
+User-agent: ExampleBrowser/1.0
+Accept: */*
+"""
 response = Servy.Handler.handle(request)
 IO.puts response
